f2a5abb523c244389113b653ed20252fbcec5251
n3k0fi5t <r05922078@ntu.edu.tw>
2021-04-23 11:10:06 +0800

    add trigger for tsvector column
    Signed-off-by: n3k0fi5t <r05922078@ntu.edu.tw>

--- pg/init.sql
+++ pg/init.sql
@@ -1,3 +1,13 @@
 CREATE EXTENSION zhparser;
 CREATE TEXT SEARCH CONFIGURATION chinese (PARSER = zhparser);
 ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l WITH simple;
+
+CREATE TRIGGER vector_column_trigger
+BEFORE INSERT OR UPDATE OF title, content, tsvector
+ON ptt_post
+FOR EACH ROW EXECUTE PROCEDURE
+tsvector_update_trigger(
+tsvector, 'public.chinese', title, content
+);
+
+UPDATE ptt_post SET tsvector = NULL;
